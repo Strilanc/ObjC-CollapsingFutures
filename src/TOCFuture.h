af50ac44371098d7b8b105498d6a1a1a873da6d8
Twisted Oak Studios Mac Mini <minitwist@Twisteds-Mac-mini.local>
2013-10-10 23:25:19 -0300

    Improving documentation

--- src/TOCFuture.h
+++ src/TOCFuture.h
@@ -2,148 +2,234 @@
 
 @class TOCFuture;
 
-/// @abstract The type of block passed to future 'finallyDo'.
+/*!
+ * The type of block passed to TOCFuture's finallyDo method.
+ * The future given to the block is required to have succeeded or failed (i.e. to not be incomplete).
+ */
 typedef void (^TOCFutureFinallyHandler)(TOCFuture * completed);
-/// @abstract The type of block passed to future 'thenDo'.
+/*!
+ * The type of block passed to TOCFuture's thenDo method.
+ * The value given to the block is a future's result.
+ */
 typedef void (^TOCFutureThenHandler)(id value);
-/// @abstract The type of block passed to future 'catchDo'.
+/*!
+ * The type of block passed to TOCFuture's catchDo method.
+ * The value given to the block is a future's failure.
+ */
 typedef void (^TOCFutureCatchHandler)(id failure);
 
-/// @abstract The type of block passed to future 'finally'.
+/*!
+ * The type of block passed to TOCFuture's finally method.
+ * The future given to the block is required to have succeeded or failed (i.e. to not be incomplete).
+ */
 typedef id (^TOCFutureFinallyContinuation)(TOCFuture * completed);
-/// @abstract The type of block passed to future 'then'.
+/*!
+ * The type of block passed to TOCFuture's then method.
+ * The value given to the block is a future's result.
+ */
 typedef id (^TOCFutureThenContinuation)(id value);
-/// @abstract The type of block passed to future 'catch'.
+/*!
+ * The type of block passed to TOCFuture's catch method.
+ * The value given to the block is a future's failure.
+ */
 typedef id (^TOCFutureCatchContinuation)(id failure);
 
-/// @abstract A TOCFuture is an eventual value. It will eventuall contain a result or a failure.
-///
-/// @discussion
-/// TOCFuture is thread-safe. It can be accessed from multiple threads concurrently.
-///
-/// Use the then/catch/finally methods to continue with a block once a TOCFuture has a value.
-/// You get a future for the continuation's completion when using then/catch/finally, allowing you to chain computations together.
-/// The continuation block is called either on the thread registering the continuation or on the thread computing the future's result.
-///
-/// You can use isIncomplete/hasResult/hasFailed to determine if the future has already completed or not.
-/// You can use forceGetResult/forceGetFailure to attempt to directly retrieve the future's result or failure.
-/// If the future has not yet completed, using forceGetX will raise an exception.
-///
-/// TOCFuture is auto-collapsing/flattening: you will never see a TOCFuture with a result of type TOCFuture because it gets flattened.
-/// For example, [TOCFuture futureWithResult:[TocFuture futureWithResult:@1]] is equivalent to [TocFuture futureWithResult:@1].
-/// Note that automatic flattening does not apply to failures. A TOCFuture's failure may be a TOCFuture.
-///
-/// Use the TOCFutureSource class to control your own TOCFuture instances.
+/*!
+ * An eventual value that either succeeds with a result, or fails with a failure.
+ *
+ * @discussion TOCFuture is thread-safe.
+ * It can be accessed from multiple threads concurrently.
+ *
+ * TOCFuture is auto-collapsing/flattening.
+ * Any TOCFuture that would have had a result of type TOCFuture gets flattened and effectively becomes its result.
+ * For example, [TOCFuture futureWithResult:[TocFuture futureWithResult:@1]] is equivalent to [TocFuture futureWithResult:@1].
+ *
+ * Note that automatic flattening does not apply to failures.
+ * A TOCFuture's failure may be a TOCFuture.
+ *
+ * Use the then/catch/finally methods to continue with a block once a TOCFuture has a value.
+ * They each return a TOCFuture for the continuation's completion, allowing you to chain computations together.
+ *
+ * You can use isIncomplete/hasResult/hasFailed to determine if the future has already completed or not.
+ * Use forceGetResult/forceGetFailure to get the future's result or failure, or an exception if the future is in the wrong state.
+ *
+ * Use the TOCFutureSource class to control your own TOCFuture instances.
+ */
 @interface TOCFuture : NSObject
 
-/// @abstract Creates and returns a future that has already completed with the given result value.
-/// @param resultValue The result the future should end up with.
-/// Nil is a valid result value.
-/// TOCFutures are valid result values, but trigger automatic collapsing.
-/// @discussion Using a future result value causes the argument to be returned, instead of a new future.
+/*!
+ * Returns a completed future containing the given result.
+ *
+ * @param resultValue The result for the returned future.
+ * Allowed to be nil.
+ * Allowed to be a future.
+ *
+ * @result A future containing the given result or else, if the given result is a future, collapses to returning the given result.
+ *
+ * @discussion
+ * When the given resultValue is a future, automatic collapse makes the returned future equivalent to the given future.
+*/
 +(TOCFuture *)futureWithResult:(id)resultValue;
 
-/// @abstract Creates and returns a future that has already failed with the given failure value.
-/// @param failureValue The failure the future should end up with.
-/// Nil is a valid failure value.
-/// TOCFutures are valid failure values, but are NOT UNWRAPPED like they are when used as results.
+/*!
+ * Returns a failed future containing the given failure.
+ *
+ * @param failureValue The failure for the returned future.
+ * Allowed to be nil.
+ * Allowed to be a future.
+ *
+ * @result A future containing the given failure.
+ *
+ * @discussion
+ * Note that automatic collapse does not apply to failures.
+ * The returned future will not be flattened even if the given failure is itself a future.
+ */
 +(TOCFuture *)futureWithFailure:(id)failureValue;
 
-/// @abstract Determines if this future has not yet completed or failed.
+/*!
+ * Determines if the receiving future has not yet completed or failed.
+ */
 -(bool)isIncomplete;
 
-/// @abstract Determines if this future has completed with a result, as opposed to having failed or still being incomplete.
+/*!
+ * Determines if the receiving future has completed with a result, as opposed to having failed or still being incomplete.
+ */
 -(bool)hasResult;
 
-/// @abstract Determines if this future has failed, as opposed to having completed with a result or still being incomplete.
+/*!
+ * Determines if the receiving future has failed, as opposed to having completed with a result or still being incomplete.
+ */
 -(bool)hasFailed;
 
-/// @abstract Accesses this future's result, if it has one. Otherwise raises an exception.
+/*!
+ * Returns the receiving future's result, unless it doesn't have one in which case an exception is raised.
+ *
+ * @pre hasResult must be true
+ *
+ * @result The future's result.
+ *
+ * @discussion
+ * Raises an exception immediately, without waiting, if the future is still incomplete.
+ */
 -(id)forceGetResult;
 
-/// @abstract Accesses this future's failure, if it has one. Otherwise raises an exception.
+/*!
+ * Returns the receiving future's failure, unless it doesn't have one in which case an exception is raised.
+ *
+ * @pre hasFailed must be true
+ *
+ * @result The future's failure.
+ *
+ * @discussion
+ * Raises an exception immediately, without waiting, if the future is still incomplete.
+ */
 -(id)forceGetFailure;
 
-/// @abstract Registers a continuation to run when this future completes with a result, exposing the eventual result as a future.
-/// @result A future for the eventual result of running the continuation on this future's result.
-/// If this future fails then the continuation is not run, and the failure is propagated to the returned future.
-/// @discussion If this future has already completed the continuation is run inline.
-/// When this future completes, the continuation will be run inline.
-/// If this future fails then the continuation is not run, and the failure is propagated to the returned future.
-/// If the continuation returns a future, automatic collapse is triggered and the returned future will match it instead of having it as a result.
+/*!
+ * Eventually evaluates a 'then' continuation on the receiving future's result, or else propagates the receiving future's failure.
+ *
+ * @result A future for the eventual result of evaluating the given 'then' block on the receiving future's result, or else a failure if the receiving future fails.
+ *
+ * @discussion If the receiving future has already succeeded with a result, the continuation is run inline.
+ *
+ * If the receiving future fails, instead of succeeding with a result, the continuation is not run and the failure is propagated into the returned future.
+ *
+ * If the continuation returns a future, instead of a normal value, then this method's result is automatically flattened to match that future instead of containing it.
+ */
 -(TOCFuture *)then:(TOCFutureThenContinuation)resultContinuation;
 
-/// @abstract Registers a continuation to run when this future fails, exposing the eventual result as a future.
-/// @result A future for the eventual result of running the continuation on this future's failure value.
-/// If this future completes with a result, the continuation is not run and the returned future gets the same result.
-/// @discussion If this future has already failed the continuation is run inline.
-/// When this future fails, the continuation will be run inline.
-/// If this future completes with a result, the continuation is not run and the returned future gets the same result.
-/// If the continuation returns a future, automatic collapse is triggered and the returned future will match it instead of having it as a result.
+/*!
+ * Eventually matches the receiving future's result, or else evaluates a 'catch' continuation on the receiving future's failure.
+ *
+ * @result A future for the eventual result of the receiving future, or else the eventual result of running the receiving future's failure through the given 'catch' block.
+ *
+ * @discussion If the receiving future has already failed, the continuation is run inline.
+ *
+ * If the continuation returns a future, instead of a normal value, then this method's result is automatically flattened to match that future instead of containing it.
+ */
 -(TOCFuture *)catch:(TOCFutureCatchContinuation)failureContinuation;
 
-/// @abstract Registers a continuation to run when this future fails or completes with a result, exposing the eventual result as a future.
-/// @result A future for the eventual result of running the continuation after this future has completed or failed.
-/// @discussion If this future has already completed or failed, the continuation is run inline.
-/// When this future completes or fails, the continuation will be run inline.
-/// If the continuation returns a future, automatic collapse is triggered and the returned future will match it instead of having it as a result.
+/*!
+ * Eventually evaluates a 'finally' continuation on the receiving future, once it has completed with a result or failed.
+ *
+ * @result A future for the eventual result of evaluating the given 'finally' block on the receiving future once it has completed.
+ *
+ * @discussion If the receiving future has already completed, the continuation is run inline.
+ *
+ * If the continuation returns a future, instead of a normal value, then this method's result is automatically flattened to match that future instead of containing it.
+ */
 -(TOCFuture *)finally:(TOCFutureFinallyContinuation)completionContinuation;
 
-/// @abstract Registers a handler to run when this future completes with a result.
-/// @discussion If this future has already completed, the handler is run inline.
-/// When this future completes, the handler will be run inline.
-/// If this future fails then the handler is not run.
+/*!
+ * Eventually runs a 'then' handler on the receiving future's result.
+ *
+ * @discussion If the receiving future has already succeeded with a result, the handler is run inline.
+ *
+ * If the receiving future fails, instead of succeeding with a result, the handler is not run.
+ */
 -(void) thenDo:(TOCFutureThenHandler)resultHandler;
 
-/// @abstract Registers a handler to run when this future fails.
-/// @discussion If this future has already failed, the handler is run inline.
-/// When this future fails, the handler will be run inline.
-/// If this future does not fail then the handler is not run.
+/*!
+ * Eventually runs a 'catch' handler on the receiving future's failure.
+ *
+ * @discussion If the receiving future has already failed, the handler is run inline.
+ *
+ * If the receiving future succeeds with a result, instead of failing, the handler is not run.
+ */
 -(void) catchDo:(TOCFutureCatchHandler)failureHandler;
 
-/// @abstract Registers a handler to run when this future completes or fails.
-/// @discussion If this future has already completed or failed, the handler is run inline.
-/// When this future completes or fails, the handler will be run inline.
+/*!
+ * Eventually runs a 'finally' handler on the receiving future, once it has completed with a result or failed.
+ *
+ * @discussion If the receiving future has already completed with a result or failed, the handler is run inline.
+ */
 -(void) finallyDo:(TOCFutureFinallyHandler)completionHandler;
 
 @end
 
-/// @abstract A TOCFutureSource is a future that can be manually given a result or failure.
-///
-/// @discussion
-/// TOCFutureSource is thread-safe.
-/// Use trySetResult and trySetFailure to cause the source's future to complete.
-/// Giving a future to trySetResult triggers automatic collapse: the source's future to become 'the same', matching the given future's result or failure.
-/// Giving a future to trySetFailure does not trigger automatic collapse.
+/*!
+ * An eventual value that can be manually completed. Extends TOCFuture.
+ *
+ * @discussion
+ * TOCFutureSource is thread-safe.
+ * It can be accessed and controlled from multiple threads concurrently.
+ *
+ * Use trySetResult/trySetFailure to cause the future to complete with a result or fail with a failure.
+ */
 @interface TOCFutureSource : TOCFuture
 
-/// @abstract Attempts to set the source's future to complete with the given result value, unwrapping the value if it's a future.
-///
-/// @result Returns true when the source's future has been successfully set, and false when it was already set.
-///
-/// @param finalResult The result value to use.
-/// TOCFutures are valid result values, but are automatically unwrapped.
-/// Nil is a valid result value.
-///
-/// @discussion
-/// No effect when the source's future was already set.
-///
-/// Automatically collapses nested futures. If the result value is a future F then:
-/// - While F is incomplete, the source's future is incomplete (but still set).
-/// - If/When F has completed with result X, the source's future has also completed with result X.
-/// - If/When F has failed with failure X, the source's future has also failed with failure X.
+/*!
+ * Attempts to set the receiving future source to complete with the given result.
+ *
+ * @result True when the future source was successfully set, and false when it was already set.
+ *
+ * @param finalResult The result the receiving future source should complete with.
+ * Allowed to be nil.
+ * Allowed to be a future.
+ *
+ * @discussion
+ * If the receiving future source has already been set, this method has no effect and returns false.
+ *
+ * If the given result is a future and this method succeeds, then the receiving future source will collapse to match the future instead of containing it.
+ *
+ * When the future source is set to match an incomplete future, it remains incomplete (but still set) until that future completes.
+ */
 -(bool) trySetResult:(id)finalResult;
 
-/// @abstract Attempts to set the source's future to fail with the given failure value.
-///
-/// @result Returns true when the source's future has been successfully set, and false when it was already set.
-///
-/// @param finalFailure The failure value to use.
-/// Nil is a valid failure value.
-/// TOCFutures are valid failure values, but are NOT UNWRAPPED like they are when used as results.
-///
-/// @discussion
-/// No effect when the source's future was already set.
+/*!
+ * Attempts to set the receiving future source to fail with the given failure.
+ *
+ * @result True when the future source was successfully set, and false when it was already set.
+ *
+ * @param finalFailure The failure the receiving future source should fail with.
+ * Allowed to be nil.
+ * Allowed to be a future.
+ *
+ * @discussion
+ * If the receiving future source has already been set, this method has no effect and returns false.
+ *
+ * No automatic collapse occurs when the given failure is a future. The receiving future source will just contain a failure that is a future, instead of matching that future.
+ */
 -(bool) trySetFailure:(id)finalFailure;
 
 @end
