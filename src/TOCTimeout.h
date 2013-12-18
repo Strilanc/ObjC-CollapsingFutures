eb6993818ea96617ee8dc10b147f98272570fd96
Craig Gidney <craig.gidney@gmail.com>
2013-12-17 23:36:07 -0400

    v0.9.0

--- src/TOCTimeout.h
+++ src/TOCTimeout.h
@@ -1,7 +1,7 @@
 #import <Foundation/Foundation.h>
 
 /*!
- * Instances of TOCTimeout as used to indicate a timeout failure occurred (e.g. by being the failure stored in a TOCFuture).
+ * Instances of TOCTimeout are used to indicate a timeout failure occurred (e.g. by being the failure stored in a TOCFuture).
  */
 @interface TOCTimeout : NSObject
 
